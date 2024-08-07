c3b26325d9210b4b76fb101083311feca04f1a1a
Antonino Vacalebre <vacalebre.antonino@gmail.com>
2024-08-07 15:23:15 +0200

    added example script

--- examples/ips_example/ips_example.ino
+++ examples/ips_example/ips_example.ino
@@ -1,12 +1,52 @@
 #include <Arduino.h>
+#include <Wire.h>
+
 #include <IPS2550.h>
+
+#define SERIAL_BAUDRATE 115200
+
+#define IPS_I2C_ADDR 24
+#define IPS_I2C_FREQ 100000
+
+#define PIN_RX1 A0
+#define PIN_RX2 A1
+#define PIN_REF A2
+
+IPS2550 ips;
+
+double get_rx1();
+double get_rx2();
+double estimate_vtx();
 
 void setup()
 {
-    // todo
+    Serial.begin(SERIAL_BAUDRATE);
+
+    Wire.setClock(IPS_I2C_FREQ);
+    Wire.begin();
+
+    ips.init(Wire, IPS_I2C_ADDR);
+
+    ips.set_voltage(VDD_3V3);
+    ips.set_automatic_gain_control(false);
+    ips.set_master_gain_boost(true);
+    ips.set_master_gain_code(48);
+    ips.set_current_bias(0xFF);
+    ips.set_output_mode(SINGLE_ENDED);
 }
 
 void loop()
 {
-    // todo
+    double freq = ips.get_tx_frequency();
+    Serial.println(freq);
+}
+
+double get_rx1()
+{
+    return (analogRead(PIN_RX1) - analogRead(PIN_REF)) / 1023 * 3.3;
+}
+
+double get_rx2()
+{
+    return (analogRead(PIN_RX2) - analogRead(PIN_REF)) / 1023 * 3.3;
 }
